546aab5f2f9ea4cfd6dde99f4366494a1f8bfa27
Tony Dong <bell-1985@163.com>
2019-11-08 11:25:00 +0800

    Update answer.sql

--- 16/answer.sql
+++ 16/answer.sql
@@ -5,12 +5,23 @@
 -- calendar 和 employee 连接，获取每个员工应该打卡的日期；
 -- 再和 attendance 左连接，获取每个员工九点之前上班打卡，下午六点之后下班打卡的记录；
 -- 然后利用 WHERE 中找出缺失的打卡记录，就是员工的缺勤记录。
+-- Oracle、MySQL 以及 PostgreSQL 实现
 SELECT c.calendar_date,e.emp_name
   FROM calendar c
   JOIN employee e ON (c.is_work_day = 'Y')
   LEFT JOIN attendance a ON (a.emp_id = e.emp_id AND a.check_date = c.calendar_date
                              AND EXTRACT(hour FROM a.clock_in) < 9
                              AND EXTRACT(hour FROM a.clock_out) >= 18)
+ WHERE a.emp_id IS NULL
+ ORDER BY c.calendar_date;
+
+-- SQL Server 实现
+SELECT c.calendar_date,e.emp_name
+  FROM calendar c
+  JOIN employee e ON (c.is_work_day = 'Y')
+  LEFT JOIN attendance a ON (a.emp_id = e.emp_id AND a.check_date = c.calendar_date
+                             AND DATEPART(hour, a.clock_in) < 9
+                             AND DATEPART(hour, a.clock_out) >= 18)
  WHERE a.emp_id IS NULL
  ORDER BY c.calendar_date;
 
