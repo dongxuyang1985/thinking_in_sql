9bf686bec3c462abaa476735ba3d00d3127944ae
Tony Dong <bell-1985@163.com>
2019-11-13 10:27:43 +0800

    Update answer.sql

--- 25/answer.sql
+++ 25/answer.sql
@@ -2,3 +2,8 @@
 -- 各种数据库还支持许多其他的数据类型，例如 XML、JSON 等。说说你使用的数据库中还有哪些常用的数据类型，有哪些应用场景和注意事项。
 
 -- 欢迎大家在专栏中留言讨论！
+
+[Oracle database 18c SQL 数据类型](https://docs.oracle.com/en/database/oracle/oracle-database/18/sqlrf/Data-Types.html#GUID-A3C0D836-BADB-44E5-A5D4-265BA5968483)
+[MySQL 8.0 数据类型](https://dev.mysql.com/doc/refman/8.0/en/data-types.html)
+[SQL Server 2017 数据类型](https://docs.microsoft.com/en-us/sql/t-sql/data-types/data-types-transact-sql?view=sql-server-2017)
+[PostgreSQL 10 数据类型](https://www.postgresql.org/docs/10/static/datatype.html)
