79d3823d86d5bfec9ba8d8c5ad34b1a84641e176
Tony Dong <bell-1985@163.com>
2019-12-20 17:05:33 +0800

    Update MySQLInsert.java

--- 37/MySQLInsert.java
+++ 37/MySQLInsert.java
@@ -7,7 +7,7 @@ import java.sql.*;
 import java.util.Properties;
 
 public class MySQLInsert {
-    public static <conn> void main(String[] args )
+    public static void main(String[] args )
     {
         String url = null;
         String user = null;
