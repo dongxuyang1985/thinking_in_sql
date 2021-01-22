a4e48613d6d7b70e5f8fd724f5bb480797809058
Tony Dong <bell-1985@163.com>
2021-01-22 21:16:10 +0800

    Create emp_sales.sql

---
+++ emp_sales.sql
@@ -0,0 +1,50 @@
+CREATE TABLE emp_sales(
+  emp_id     INTEGER NOT NULL,
+  sale_year  INTEGER NOT NULL,
+  sale_month INTEGER NOT NULL,
+  amount     NUMERIC(8,2) NOT NULL,
+  CONSTRAINT pk_emp_sales PRIMARY KEY (emp_id, sale_year, sale_month)
+);
+
+INSERT INTO emp_sales (emp_id,sale_year,sale_month,amount) VALUES (19,2021,1,15672.53);
+INSERT INTO emp_sales (emp_id,sale_year,sale_month,amount) VALUES (20,2021,1,11160.46);
+INSERT INTO emp_sales (emp_id,sale_year,sale_month,amount) VALUES (21,2021,1,13763.75);
+INSERT INTO emp_sales (emp_id,sale_year,sale_month,amount) VALUES (22,2021,1,11210.34);
+INSERT INTO emp_sales (emp_id,sale_year,sale_month,amount) VALUES (23,2021,1,14610.88);
+INSERT INTO emp_sales (emp_id,sale_year,sale_month,amount) VALUES (24,2021,1,13747.64);
+INSERT INTO emp_sales (emp_id,sale_year,sale_month,amount) VALUES (25,2021,1,12816.20);
+INSERT INTO emp_sales (emp_id,sale_year,sale_month,amount) VALUES (19,2021,2,14413.77);
+INSERT INTO emp_sales (emp_id,sale_year,sale_month,amount) VALUES (20,2021,2,14266.04);
+INSERT INTO emp_sales (emp_id,sale_year,sale_month,amount) VALUES (21,2021,2,16984.42);
+INSERT INTO emp_sales (emp_id,sale_year,sale_month,amount) VALUES (22,2021,2,15272.97);
+INSERT INTO emp_sales (emp_id,sale_year,sale_month,amount) VALUES (23,2021,2,16103.39);
+INSERT INTO emp_sales (emp_id,sale_year,sale_month,amount) VALUES (24,2021,2,15465.84);
+INSERT INTO emp_sales (emp_id,sale_year,sale_month,amount) VALUES (25,2021,2,14824.55);
+INSERT INTO emp_sales (emp_id,sale_year,sale_month,amount) VALUES (19,2021,3,14352.10);
+INSERT INTO emp_sales (emp_id,sale_year,sale_month,amount) VALUES (20,2021,3,15116.63);
+INSERT INTO emp_sales (emp_id,sale_year,sale_month,amount) VALUES (21,2021,3,13766.24);
+INSERT INTO emp_sales (emp_id,sale_year,sale_month,amount) VALUES (22,2021,3,15410.53);
+INSERT INTO emp_sales (emp_id,sale_year,sale_month,amount) VALUES (23,2021,3,16377.44);
+INSERT INTO emp_sales (emp_id,sale_year,sale_month,amount) VALUES (24,2021,3,14856.07);
+INSERT INTO emp_sales (emp_id,sale_year,sale_month,amount) VALUES (25,2021,3,16371.59);
+INSERT INTO emp_sales (emp_id,sale_year,sale_month,amount) VALUES (19,2021,4,16660.24);
+INSERT INTO emp_sales (emp_id,sale_year,sale_month,amount) VALUES (20,2021,4,17730.30);
+INSERT INTO emp_sales (emp_id,sale_year,sale_month,amount) VALUES (21,2021,4,17898.77);
+INSERT INTO emp_sales (emp_id,sale_year,sale_month,amount) VALUES (22,2021,4,18044.52);
+INSERT INTO emp_sales (emp_id,sale_year,sale_month,amount) VALUES (23,2021,4,18061.82);
+INSERT INTO emp_sales (emp_id,sale_year,sale_month,amount) VALUES (24,2021,4,18744.78);
+INSERT INTO emp_sales (emp_id,sale_year,sale_month,amount) VALUES (25,2021,4,14441.28);
+INSERT INTO emp_sales (emp_id,sale_year,sale_month,amount) VALUES (19,2021,5,19108.85);
+INSERT INTO emp_sales (emp_id,sale_year,sale_month,amount) VALUES (20,2021,5,19466.56);
+INSERT INTO emp_sales (emp_id,sale_year,sale_month,amount) VALUES (21,2021,5,16984.75);
+INSERT INTO emp_sales (emp_id,sale_year,sale_month,amount) VALUES (22,2021,5,17029.43);
+INSERT INTO emp_sales (emp_id,sale_year,sale_month,amount) VALUES (23,2021,5,18389.50);
+INSERT INTO emp_sales (emp_id,sale_year,sale_month,amount) VALUES (24,2021,5,15220.30);
+INSERT INTO emp_sales (emp_id,sale_year,sale_month,amount) VALUES (25,2021,5,18506.34);
+INSERT INTO emp_sales (emp_id,sale_year,sale_month,amount) VALUES (19,2021,6,20154.83);
+INSERT INTO emp_sales (emp_id,sale_year,sale_month,amount) VALUES (20,2021,6,17350.87);
+INSERT INTO emp_sales (emp_id,sale_year,sale_month,amount) VALUES (21,2021,6,16034.62);
+INSERT INTO emp_sales (emp_id,sale_year,sale_month,amount) VALUES (22,2021,6,16308.70);
+INSERT INTO emp_sales (emp_id,sale_year,sale_month,amount) VALUES (23,2021,6,19910.08);
+INSERT INTO emp_sales (emp_id,sale_year,sale_month,amount) VALUES (24,2021,6,16072.75);
+INSERT INTO emp_sales (emp_id,sale_year,sale_month,amount) VALUES (25,2021,6,18373.68);
